884d6ea4e05b8912cf335dee058c46d324979613
Hypekage <jmestmnag@gmail.com>
2023-11-07 15:49:35 +0545

    reources or translation moved to locales

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,10 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
 
 i18n
+  .use(Backend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
@@ -15,30 +17,6 @@ i18n
     fallbackLng: "en",
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
-    },
-    resources: {
-      en: {
-        translation: {
-          info: {
-            one: "Practicing Localization",
-            two: "Learn React",
-          },
-          title: "English Localization",
-          counter_one: "Changed language just once",
-          counter_other: "Language has been changed {{count}} times",
-        },
-      },
-      np: {
-        translation: {
-          info: {
-            one: "स्थानीयकरण अभ्यास",
-            two: "रियाक्त जान्नुहोस्",
-          },
-          title: "नेपाली स्थानीयकरण",
-          counter_one: "भाषा एक  पटक परिवर्तन गरिएको छ",
-          counter_other: "भाषा {{count}} पटक परिवर्तन गरिएको छ",
-        },
-      },
     },
   });
 
